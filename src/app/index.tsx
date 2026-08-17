8d26dba3c701d8d4e2f01827b22239af3bebdff8
willy <zhmsistemas@gmail.com>
2026-08-16 19:55:56 -0500

    correciones inicio

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,19 @@
-import { Text, View } from "react-native";
+import { StatusBar, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 import "../global.css";
 
 export default function HomeScreen() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
       <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+        Bienvenido a Llévame!
       </Text>
-    </View>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </SafeAreaView>
   );
 }
