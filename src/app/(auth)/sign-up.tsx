e6b07848c1cebee98b91fc9ead9aa7f38e95e804
willy <zhmsistemas@gmail.com>
2026-08-17 20:21:08 -0500

    agrega carpetas y archivos

---
+++ src/app/(auth)/sign-up.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function Signup() {
+  return (
+    <SafeAreaView className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold text-blue-500">Sign Up</Text>
+    </SafeAreaView>
+  );
+}
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,6 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-
+import { Redirect } from "expo-router";
 import "../global.css";
 
 export default function HomeScreen() {
-  return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Bienvenido a Llévame !
-      </Text>
-    </SafeAreaView>
-  );
+  return <Redirect href="/(auth)/welcome" />;
 }
